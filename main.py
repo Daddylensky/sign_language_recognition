48398952e2ee5c8eb06083c2c9d23eac3161b532
Obolensky <69239884+Daddylensky@users.noreply.github.com>
2023-10-12 16:59:13 -0400

    Normalized line endings for main.py

--- main.py
+++ main.py
@@ -1,45 +1,66 @@
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout , MaxPooling2D
-from keras.preprocessing.image import ImageDataGenerator
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report,confusion_matrix
-from keras.callbacks import ReduceLROnPlateau
-import os
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+import mediapipe as mp
+
+model = load_model('sign_language_model.h5')
+mphands = mp.solutions.hands
+hands = mphands.Hands()
+mp_drawing = mp.solutions.drawing_utils
+cap = cv2.VideoCapture(0)
+
+_, frame = cap.read()
+
+h, w, c = frame.shape
 
 
-def os_walk():
-    for dirname, _, filenames in os.walk('./dataset/'):
-        for filename in filenames:
-            print(os.path.join(dirname, filename))
+while True:
+    _, frame = cap.read()
+    framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = hands.process(framergb)
+    hand_landmarks = result.multi_hand_landmarks
+    if hand_landmarks:
+        for handLMs in hand_landmarks:
+            x_max = 0
+            y_max = 0
+            x_min = w
+            y_min = h
+            for lm in handLMs.landmark:
+                x, y = int(lm.x * w), int(lm.y * h)
+                if x > x_max:
+                    x_max = x
+                if x < x_min:
+                    x_min = x
+                if y > y_max:
+                    y_max = y
+                if y < y_min:
+                    y_min = y
+            cv2.rectangle(frame, (x_min-20, y_min-20), (x_max+20, y_max+20), (0, 255, 0), 2)
+            frame = mp_drawing.draw_landmarks(frame, handLMs, mphands.HAND_CONNECTIONS)
 
+            # Preprocess the image: resize and normalize
+            input_frame = cv2.resize(frame, (224, 224))
+            input_frame = np.expand_dims(input_frame, axis=0)
+            input_frame = input_frame / 255.0
 
+            # Make prediction
+            prediction = model.predict(input_frame)
+            predicted_class = np.argmax(prediction[0])
 
-train_datagen = ImageDataGenerator(rescale=1./255)  # Add more augmentations as needed
+            # Map the predicted class index to the corresponding letter
+            labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'space', 'del', 'nothing']  # Replace with your actual labels
+            predicted_label = labels[predicted_class]
 
-train_generator = train_datagen.flow_from_directory(
-    './dataset/asl_alphabet_train/asl_alphabet_train/',  # Replace with your folder path
-    target_size=(224, 224),  # Resize images to a standard size
-    batch_size=32,
-    class_mode='categorical'
-)
+            # Display the prediction on the frame
+            cv2.putText(frame, predicted_label, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
-    MaxPooling2D(2, 2),
-    Conv2D(64, (3, 3), activation='relu'),
-    MaxPooling2D(2, 2),
-    Flatten(),
-    Dense(512, activation='relu'),
-    Dropout(0.5),
-    Dense(29, activation='softmax')  # 29 classes based on your dataset
-])
+            # Show the frame
+            cv2.imshow("Sign Language Recognition", frame)
 
+    # Exit loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-history = model.fit(
-    train_generator,
-    steps_per_epoch=train_generator.samples // train_generator.batch_size,
-    epochs=10  # Adjust as needed
-)
-
+# Release the camera and destroy all OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
