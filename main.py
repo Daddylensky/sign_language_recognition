56174baa02a964b89208cacb6b695bde5687f046
Obolensky <69239884+Daddylensky@users.noreply.github.com>
2023-10-17 15:23:27 -0400

    there is a problem with the data given, the dataset seems to just be 1000s of frame of a video of a white person doing signs, sometimes his white skin is seen, sometimes there is dark shadows. i need better data augmentation and might add in more of my own photos

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ from tensorflow.keras.models import load_model
 import mediapipe as mp
 
 model = load_model('sign_language_model.h5')
+print("here\n\n")
+print(model.output_shape)
+print("\n\n")
 mphands = mp.solutions.hands
 hands = mphands.Hands()
 mp_drawing = mp.solutions.drawing_utils
 cap = cv2.VideoCapture(0)
 
-_, frame = cap.read()
-
-h, w, c = frame.shape
 
 
 while True:
     _, frame = cap.read()
+    h, w, c = frame.shape  # Add this line to define frame dimensions
     framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     result = hands.process(framergb)
     hand_landmarks = result.multi_hand_landmarks
@@ -27,39 +28,43 @@ while True:
             y_min = h
             for lm in handLMs.landmark:
                 x, y = int(lm.x * w), int(lm.y * h)
-                if x > x_max:
-                    x_max = x
-                if x < x_min:
-                    x_min = x
-                if y > y_max:
-                    y_max = y
-                if y < y_min:
-                    y_min = y
-            cv2.rectangle(frame, (x_min-20, y_min-20), (x_max+20, y_max+20), (0, 255, 0), 2)
-            frame = mp_drawing.draw_landmarks(frame, handLMs, mphands.HAND_CONNECTIONS)
+                x_max = max(x, x_max)
+                x_min = min(x, x_min)
+                y_max = max(y, y_max)
+                y_min = min(y, y_min)
 
-            # Preprocess the image: resize and normalize
-            input_frame = cv2.resize(frame, (224, 224))
-            input_frame = np.expand_dims(input_frame, axis=0)
-            input_frame = input_frame / 255.0
+            x_min = max(0, x_min - 20)
+            y_min = max(0, y_min - 20)
+            x_max = min(w, x_max + 20)
+            y_max = min(h, y_max + 20)
+
+            roi = frame[y_min:y_max, x_min:x_max]
+            
+            if roi.size == 0:  # Skip if the roi is empty
+                continue
+
+            input_roi = cv2.resize(roi, (224, 224))
+            input_roi = np.expand_dims(input_roi, axis=0)
+            input_roi = input_roi / 255.0
 
             # Make prediction
-            prediction = model.predict(input_frame)
+            prediction = model.predict(input_roi)
             predicted_class = np.argmax(prediction[0])
-
+            
             # Map the predicted class index to the corresponding letter
-            labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'space', 'del', 'nothing']  # Replace with your actual labels
+            labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'space', 'del', 'nothing']
             predicted_label = labels[predicted_class]
 
             # Display the prediction on the frame
             cv2.putText(frame, predicted_label, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-            # Show the frame
-            cv2.imshow("Sign Language Recognition", frame)
+    # Show the frame
+    cv2.imshow("Sign Language Recognition", frame)
 
     # Exit loop if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 
 # Release the camera and destroy all OpenCV windows
 cap.release()
